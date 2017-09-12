20e7f252b13055591ab47dd43bafe94b56dd94e7
AngiieCervantess <A01410524@itesm.mx>
2017-09-11 22:30:43 -0500

    Create ClaseNodo.cpp

---
+++ ClaseNodo.cpp
@@ -0,0 +1,23 @@
+# ACT02PARCIAL2
+Clase nodo en C++ (Completa)
+
+// NodoClass.cpp: define el punto de entrada de la aplicación de consola.
+// Angelica Cervantes A01410524
+//Estructura de Datos
+//Actividad1 Parcial2
+
+#include "stdafx.h"
+
+class nodoClass {
+
+public:
+	nodoClass(int n, nodoClass *siguiente = NULL) {
+		valor = n;
+		siguiente = siguiente;
+	}
+
+private:
+	int valor;
+	nodoClass *siguiente;
+
+};
